ea81f41bb79fc2c93ee28940f9447b1ea7c640a7
Hoan Le <hoanle@xtaypro.com>
2020-07-02 01:51:26 +0700

    google

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,7 @@ const passport = require("./../passport/index")
 const User = require('./../models/userModel')
 
 exports.loginFacebook = passport.authenticate("facebook", { scope: ['email'] });
+exports.loginGoogle = passport.authenticate("google", { scope: ['profile', 'email'] });
 
 exports.facebookAuthHandler = (request, response, next) => {
     passport.authenticate("facebook", async (err, profile) => {
@@ -13,6 +14,24 @@ exports.facebookAuthHandler = (request, response, next) => {
         }
         const { email, last_name, first_name } = profile._json
         const user = await User.findOneOrCreate(email, `${first_name} ${last_name}`)
+        const token = await user.generateToken()
+        response.status(200).json({
+            status: 'success',
+            data: { user, token }
+        })
+    })(request, response, next);
+} 
+
+exports.googleAuthHandler = (request, response, next) => {
+    passport.authenticate("google", async (err, profile) => {
+        if (err) {
+            return response.status(400).json({
+                status: 'fail',
+                message: err.message
+            })
+        }
+        const { email, name } = profile._json
+        const user = await User.findOneOrCreate(email, name)
         const token = await user.generateToken()
         response.status(200).json({
             status: 'success',
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,8 +1,12 @@
 var express = require('express')
 var router = express.Router();
-const { loginFacebook, facebookAuthHandler } = require('./../controllers/authController')
+const { loginFacebook, facebookAuthHandler, loginGoogle, googleAuthHandler } = require('./../controllers/authController')
 
 router.get("/auth/facebook/login", loginFacebook);
 router.get("/auth/facebook/authorized", facebookAuthHandler);
+
+router.get("/auth/google/login", loginGoogle);
+router.get("/auth/google/authorized", googleAuthHandler);
+
 
 module.exports = router
